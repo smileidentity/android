a51c1c00205d2eb69158962578240063d61a6d8d
Vansh Gandhi <vansh@smileidentity.com>
2022-12-21 13:25:26 -0800

    Fix ktlint

--- ui/src/main/java/com/smileidentity/ui/SmileIdentityAttribution.kt
+++ ui/src/main/java/com/smileidentity/ui/SmileIdentityAttribution.kt
@@ -3,11 +3,7 @@ package com.smileidentity.ui
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Arrangement.SpaceAround
-import androidx.compose.foundation.layout.Arrangement.SpaceEvenly
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -23,7 +19,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.smileidentity.ui.theme.SmileIdentityMediumBlue
 
 @Preview
