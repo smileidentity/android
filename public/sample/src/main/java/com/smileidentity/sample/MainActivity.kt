cc3434f0f6aa1c38090eb911c44fcda608b39d95
Vansh Gandhi <vansh@smileidentity.com>
2022-12-21 20:25:32 -0800

    Remove unused import

--- public/sample/src/main/java/com/smileidentity/sample/MainActivity.kt
+++ public/sample/src/main/java/com/smileidentity/sample/MainActivity.kt
@@ -33,7 +33,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
