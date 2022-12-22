99d0bcfd36afb9892bfc1a312666e5288df5f2b5
Vansh Gandhi <vansh@smileidentity.com>
2022-12-21 22:01:00 -0800

    Update sample app

--- public/sample/src/main/java/com/smileidentity/sample/MainActivity.kt
+++ public/sample/src/main/java/com/smileidentity/sample/MainActivity.kt
@@ -1,48 +1,57 @@
 package com.smileidentity.sample
 
+import android.content.Context
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement.SpaceBetween
-import androidx.compose.foundation.layout.Arrangement.spacedBy
+import androidx.compose.foundation.layout.Arrangement.SpaceAround
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Face
-import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.outlined.PlayArrow
 import androidx.compose.material.icons.rounded.Home
 import androidx.compose.material.icons.rounded.Info
 import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.material3.Card
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconToggleButton
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.centerAlignedTopAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment.Companion.Start
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -69,19 +78,24 @@ class MainActivity : AppCompatActivity() {
 fun MainScreen() {
     val navController = rememberNavController()
     SmileIdentityTheme {
-        Scaffold(
-            topBar = { TopBar() },
-            bottomBar = { BottomBar { navController.navigate(it) } },
-            content = {
-                Box(Modifier.padding(it)) {
-                    NavHost(navController = navController, startDestination = "home") {
-                        composable("home") { ProductSelectionScreen() }
-                        composable("resources") { ResourcesScreen() }
-                        composable("about") { AboutScreen() }
+        Surface(
+            color = MaterialTheme.colorScheme.background,
+            contentColor = MaterialTheme.colorScheme.onBackground
+        ) {
+            Scaffold(
+                topBar = { TopBar() },
+                bottomBar = { BottomBar { navController.navigate(it) } },
+                content = {
+                    Box(Modifier.padding(it)) {
+                        NavHost(navController = navController, startDestination = "home") {
+                            composable("home") { ProductSelectionScreen() }
+                            composable("resources") { ResourcesScreen() }
+                            composable("about") { AboutScreen() }
+                        }
                     }
                 }
-            }
-        )
+            )
+        }
     }
 }
 
@@ -89,45 +103,44 @@ fun MainScreen() {
 @Preview
 @Composable
 fun TopBar() {
-    TopAppBar(title = { Text(text = stringResource(R.string.app_name)) })
+    var checked by remember { mutableStateOf(false) }
+    TopAppBar(
+        title = {
+            Text(
+                stringResource(R.string.app_name),
+                color = MaterialTheme.colorScheme.onPrimary
+            )
+        },
+        actions = {
+            IconToggleButton(checked = checked, onCheckedChange = { checked = it }) {
+                val icon = if (checked) Icons.Filled.PlayArrow else Icons.Outlined.PlayArrow
+                Icon(icon, null, tint = MaterialTheme.colorScheme.onPrimary)
+            }
+        },
+        colors = centerAlignedTopAppBarColors(
+            MaterialTheme.colorScheme.primary
+        )
+    )
 }
-
-data class BottomNavItem(
-    val name: String,
-    val route: String,
-    val icon: ImageVector,
-)
 
 @Preview
 @Composable
 fun BottomBar(onDestinationSelected: (String) -> Unit = {}) {
     var selectedItem by remember { mutableStateOf(0) }
     val bottomNavItems = listOf(
-        BottomNavItem(
-            name = "Home",
-            route = "home",
-            icon = Icons.Rounded.Home,
-        ),
-        BottomNavItem(
-            name = "Resources",
-            route = "resources",
-            icon = Icons.Rounded.Info,
-        ),
-        BottomNavItem(
-            name = "About Us",
-            route = "about",
-            icon = Icons.Rounded.Settings,
-        ),
+        Triple("Home", "home", Icons.Rounded.Home),
+        Triple("Resources", "resources", Icons.Rounded.Info),
+        Triple("About Us", "about", Icons.Rounded.Settings),
     )
     NavigationBar {
         bottomNavItems.forEachIndexed { index, item ->
             NavigationBarItem(
                 selected = selectedItem == index,
-                icon = { Icon(item.icon, item.name) },
-                label = { Text(item.name) },
+                icon = { Icon(item.third, item.first) },
+                label = { Text(item.first) },
                 onClick = {
                     selectedItem = index
-                    onDestinationSelected(item.route)
+                    onDestinationSelected(item.second)
                 }
             )
         }
@@ -140,11 +153,11 @@ fun MainContent() {
     SelfieCaptureOrPermissionScreen {
         if (it is SelfieCaptureResult.Success) {
             val message = "Image captured successfully: ${it.selfieFile}"
-            Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+            context.toast(message)
             Timber.d(message)
         } else if (it is SelfieCaptureResult.Error) {
             val message = "Image capture error: $it"
-            Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+            context.toast(message)
             Timber.e(it.throwable, message)
         }
     }
@@ -153,187 +166,90 @@ fun MainContent() {
 @Preview
 @Composable
 fun ProductSelectionScreen() {
-    val products = listOf(
-        "Basic KYC",
-        "Enhanced KYC",
-        "Biometric KYC",
-        "Document Verification",
-        "SmartSelfie™ Authentication",
-    )
     val context = LocalContext.current
+    val products = listOf(
+        Pair("SmartSelfie™ Authentication") { context.toast("Clicked on Auth") },
+    )
     Column(
+        horizontalAlignment = CenterHorizontally,
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp)
     ) {
-        Text("Test Our Products")
-        LazyVerticalGrid(columns = GridCells.Fixed(2), contentPadding = PaddingValues(8.dp)) {
+        Text("Test Our Products", style = MaterialTheme.typography.headlineSmall)
+        Spacer(modifier = Modifier.height(16.dp))
+        LazyVerticalGrid(columns = GridCells.Fixed(2)) {
             items(products) {
-                Card(
-                    modifier = Modifier
-                        .padding(4.dp)
-                        .clickable {
-                            Toast
-                                .makeText(context, "TODO: launch $it", Toast.LENGTH_SHORT)
-                                .show()
-                        }
-                ) {
-                    Image(
-                        imageVector = Icons.Default.Face,
-                        contentDescription = null,
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                    Text(
-                        text = it,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(8.dp)
-                    )
+                Card(modifier = Modifier.size(96.dp).clickable(onClick = it.second)) {
+                    Column(
+                        horizontalAlignment = CenterHorizontally,
+                        verticalArrangement = SpaceAround,
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        Image(imageVector = Icons.Default.Face, contentDescription = null)
+                        Text(text = it.first, textAlign = TextAlign.Center)
+                    }
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
 fun ResourcesScreen() {
+    val resources = listOf(
+        Pair("Explore our documentation", "Read everything related to our solution stack"),
+        Pair("Privacy Policy", "Learn more about how we handle data"),
+        Pair("View FAQs", "Explore frequently asked questions"),
+        Pair("Supported ID types and documents", "See our coverage range across the continent"),
+    )
     Column(
-        horizontalAlignment = Start,
-        verticalArrangement = spacedBy(8.dp),
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
     ) {
         val context = LocalContext.current
-        Row(
-            horizontalArrangement = SpaceBetween,
-            modifier = Modifier
-                .fillMaxWidth()
-                .clickable {
-                    Toast
-                        .makeText(context, "Who we are", Toast.LENGTH_LONG)
-                        .show()
-                }
-                .padding(8.dp),
-        ) {
-            Column {
-                Text(text = "Explore our documentation")
-                Text(text = "Read everything related to our solution stack")
-            }
-            Icon(imageVector = Icons.Default.ArrowForward, contentDescription = null)
-        }
-
-        Row(
-            horizontalArrangement = SpaceBetween,
-            modifier = Modifier
-                .fillMaxWidth()
-                .clickable {
-                    Toast
-                        .makeText(context, "Visit our website", Toast.LENGTH_LONG)
-                        .show()
-                }
-                .padding(8.dp),
-        ) {
-            Column {
-                Text(text = "Privacy Policy")
-                Text(text = "Learn more about how we handle data")
-            }
-            Icon(imageVector = Icons.Default.ArrowForward, contentDescription = null)
-        }
-
-        Row(
-            horizontalArrangement = SpaceBetween,
-            modifier = Modifier
-                .fillMaxWidth()
-                .clickable {
-                    Toast
-                        .makeText(context, "Contact support", Toast.LENGTH_LONG)
-                        .show()
-                }
-                .padding(8.dp),
-        ) {
-            Column {
-                Text(text = "View FAQs")
-                Text(text = "Explore frequently asked questions")
-            }
-            Icon(imageVector = Icons.Default.ArrowForward, contentDescription = null)
-        }
-
-        Row(
-            horizontalArrangement = SpaceBetween,
-            modifier = Modifier
-                .fillMaxWidth()
-                .clickable {
-                    Toast
-                        .makeText(context, "Contact support", Toast.LENGTH_LONG)
-                        .show()
-                }
-                .padding(8.dp),
-        ) {
-            Column {
-                Text(text = "Supported ID types and documents")
-                Text(text = "See our coverage range across the continent")
-            }
-            Icon(imageVector = Icons.Default.ArrowForward, contentDescription = null)
+        resources.forEach {
+            ListItem(
+                headlineText = { Text(it.first) },
+                supportingText = { Text(it.second) },
+                trailingContent = { Icon(Icons.Default.ArrowForward, null) },
+                modifier = Modifier.clickable { context.toast(it.first) }
+            )
+            Divider()
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
 fun AboutScreen() {
+    val abouts = listOf(
+        Pair("Who we are", Icons.Default.Info),
+        Pair("Visit our website", Icons.Default.Star),
+        Pair("Contact support", Icons.Default.Email),
+    )
     Column(
-        horizontalAlignment = Start,
-        verticalArrangement = spacedBy(8.dp),
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
     ) {
         val context = LocalContext.current
-        Row(
-            horizontalArrangement = SpaceBetween,
-            modifier = Modifier
-                .fillMaxWidth()
-                .clickable {
-                    Toast
-                        .makeText(context, "Who we are", Toast.LENGTH_LONG)
-                        .show()
-                }
-                .padding(8.dp),
-        ) {
-            Icon(imageVector = Icons.Default.Info, contentDescription = null)
-            Text(text = "Who we are")
-            Icon(imageVector = Icons.Default.ArrowForward, contentDescription = null)
-        }
-
-        Row(
-            horizontalArrangement = SpaceBetween,
-            modifier = Modifier
-                .fillMaxWidth()
-                .clickable {
-                    Toast
-                        .makeText(context, "Visit our website", Toast.LENGTH_LONG)
-                        .show()
-                }
-                .padding(8.dp),
-        ) {
-            Icon(imageVector = Icons.Default.Home, contentDescription = null)
-            Text(text = "Visit our website")
-            Icon(imageVector = Icons.Default.ArrowForward, contentDescription = null)
-        }
-
-        Row(
-            horizontalArrangement = SpaceBetween,
-            modifier = Modifier
-                .fillMaxWidth()
-                .clickable {
-                    Toast
-                        .makeText(context, "Contact support", Toast.LENGTH_LONG)
-                        .show()
-                }
-                .padding(8.dp),
-        ) {
-            Icon(imageVector = Icons.Default.Email, contentDescription = null)
-            Text(text = "Contact support")
-            Icon(imageVector = Icons.Default.ArrowForward, contentDescription = null)
+        abouts.forEach {
+            ListItem(
+                headlineText = { Text(it.first) },
+                leadingContent = { Icon(it.second, null) },
+                trailingContent = { Icon(Icons.Default.ArrowForward, null) },
+                modifier = Modifier.clickable { context.toast(it.first) }
+            )
+            Divider()
         }
     }
+}
+
+private fun Context.toast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 }
