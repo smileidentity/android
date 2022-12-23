aef2fd68055a1f3ddd5ae2f32e670397f3f4400a
Vansh Gandhi <vansh@smileidentity.com>
2022-12-23 14:04:43 -0800

    Implement Resources screen

--- public/sample/src/main/java/com/smileidentity/sample/ResourcesScreen.kt
+++ public/sample/src/main/java/com/smileidentity/sample/ResourcesScreen.kt
@@ -14,7 +14,7 @@ import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 
@@ -22,36 +22,36 @@ import androidx.compose.ui.tooling.preview.Preview
 @Preview
 @Composable
 fun ResourcesScreen() {
+    val uriHandler = LocalUriHandler.current
     val resources = listOf(
-        Pair(
+        Triple(
             stringResource(R.string.resources_explore_docs_title),
             stringResource(R.string.resources_explore_docs_subtitle),
-        ),
-        Pair(
+        ) { uriHandler.openUri("https://docs.smileidentity.com") },
+        Triple(
             stringResource(R.string.resources_privacy_policy_title),
             stringResource(R.string.resources_privacy_policy_subtitle),
-        ),
-        Pair(
+        ) { uriHandler.openUri("https://smileidentity.com/privacy-policy") },
+        Triple(
             stringResource(R.string.resources_faqs_title),
             stringResource(R.string.resources_faqs_subtitle),
-        ),
-        Pair(
+        ) { uriHandler.openUri("https://docs.smileidentity.com/further-reading/faqs") },
+        Triple(
             stringResource(R.string.resources_supported_types_title),
             stringResource(R.string.resources_supported_types_subtitle),
-        ),
+        ) { uriHandler.openUri("https://docs.smileidentity.com/supported-id-types") },
     )
     Column(
         modifier = Modifier
             .fillMaxSize()
             .verticalScroll(rememberScrollState()),
     ) {
-        val context = LocalContext.current
         resources.forEach {
             ListItem(
                 headlineText = { Text(it.first) },
                 supportingText = { Text(it.second) },
                 trailingContent = { Icon(Icons.Default.ArrowForward, null) },
-                modifier = Modifier.clickable { context.toast(it.first) },
+                modifier = Modifier.clickable { it.third() },
             )
             Divider()
         }
