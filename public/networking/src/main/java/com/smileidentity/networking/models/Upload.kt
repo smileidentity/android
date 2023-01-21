51041efd5df208714944b72b91632489a6647f22
Vansh Gandhi <vansh@smileidentity.com>
2023-01-20 17:09:26 -0800

    Add version names

--- public/networking/src/main/java/com/smileidentity/networking/models/Upload.kt
+++ public/networking/src/main/java/com/smileidentity/networking/models/Upload.kt
@@ -9,8 +9,7 @@ import java.io.File
 @JsonClass(generateAdapter = true)
 data class UploadRequest(
     @Json(name = "images") val images: List<UploadImageInfo>,
-    @Json(name = "package_information") val packageInformation: UploadPackageInformation =
-        UploadPackageInformation(),
+    @Json(name = "package_information") val packageInfo: UploadPackageInfo = UploadPackageInfo(),
 )
 
 @JsonClass(generateAdapter = true)
@@ -46,8 +45,9 @@ enum class ImageType {
 }
 
 @JsonClass(generateAdapter = true)
-data class UploadPackageInformation(
+data class UploadPackageInfo(
     @Json(name = "apiVersion") val apiVersion: ApiVersion = ApiVersion(),
+    @Json(name = "version_names") val versionNames: VersionNames = VersionNames(),
 )
 
 @JsonClass(generateAdapter = true)
@@ -55,4 +55,12 @@ data class ApiVersion(
     @Json(name = "buildNumber") val buildNumber: Int = 2,
     @Json(name = "majorVersion") val majorVersion: Int = 2,
     @Json(name = "minorVersion") val minorVersion: Int = 1,
+)
+
+@JsonClass(generateAdapter = true)
+data class VersionNames(
+    // TODO: Grab the version once build.gradle is updated with versioning
+    @Json(name = "sid_sdk_version") val version: String = "2.0.0",
+    @Json(name = "sid_sdk_type") val type: String = "Android",
+    @Json(name = "sid_sdk_ux_version") val uxVersion: String = "1.0",
 )
