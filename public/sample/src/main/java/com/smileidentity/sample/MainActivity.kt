b2693f901f3941273fbefefc23c731cceeac1276
Vansh Gandhi <vansh@smileidentity.com>
2023-02-01 09:09:58 -0800

    Use transparent nav bar (#12)

--- public/sample/src/main/java/com/smileidentity/sample/MainActivity.kt
+++ public/sample/src/main/java/com/smileidentity/sample/MainActivity.kt
@@ -3,10 +3,12 @@ package com.smileidentity.sample
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent { MainScreen() }
     }
 }
