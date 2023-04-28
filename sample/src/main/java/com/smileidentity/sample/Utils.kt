8b07689c356df58bc9c64570bb9347c69affd0fe
Vansh Gandhi <vansh@smileidentity.com>
2023-04-28 10:11:08 -0400

    Copy User ID after Registration (#49)

--- sample/src/main/java/com/smileidentity/sample/Utils.kt
+++ sample/src/main/java/com/smileidentity/sample/Utils.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.widget.Toast
 import androidx.annotation.StringRes
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
@@ -15,6 +16,16 @@ fun Context.toast(@StringRes message: Int) {
     Toast.makeText(this, message, Toast.LENGTH_LONG).show()
 }
 
-fun SnackbarHostState.showSnackbar(scope: CoroutineScope, message: String) {
-    scope.launch { showSnackbar(message) }
+fun SnackbarHostState.showSnackbar(
+    scope: CoroutineScope,
+    message: String,
+    actionLabel: String? = null,
+    action: (() -> Unit)? = null,
+) {
+    scope.launch {
+        val result = showSnackbar(message, actionLabel, withDismissAction = true)
+        if (result == SnackbarResult.ActionPerformed) {
+            action?.invoke()
+        }
+    }
 }
