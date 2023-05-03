0c08762aef37fdbbc606248843d89c013c223628
Vansh Gandhi <vansh@smileidentity.com>
2023-05-03 17:00:39 -0400

    DocumentCaptureInstructionScreenTest

--- lib/src/androidTest/java/com/smileidentity/compose/DocumentCaptureInstructionScreenTest.kt
+++ lib/src/androidTest/java/com/smileidentity/compose/DocumentCaptureInstructionScreenTest.kt
@@ -1,8 +1,9 @@
 package com.smileidentity.compose
 
 import android.Manifest
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.GrantPermissionRule
@@ -29,9 +30,47 @@ class DocumentCaptureInstructionScreenTest {
         composeTestRule.setContent {
             DocumentCaptureInstructionsScreen(onInstructionsAcknowledgedTakePhoto = onTakePhoto)
         }
-        composeTestRule.onNodeWithTag("take-photo-button").performClick()
+        composeTestRule.onNodeWithText("Take Photo").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Take Photo").performClick()
+        composeTestRule.waitForIdle()
 
         // then
         assertTrue(callbackInvoked)
+    }
+
+    @Test
+    fun shouldInvokeUploadPhotoCallbackOnButtonClick() {
+        // given
+        var callbackInvoked = false
+        val onUploadPhoto = { callbackInvoked = true }
+
+        // when
+        composeTestRule.setContent {
+            DocumentCaptureInstructionsScreen(
+                allowPhotoFromGallery = true,
+                onInstructionsAcknowledgedSelectFromGallery = onUploadPhoto,
+                onInstructionsAcknowledgedTakePhoto = { },
+            )
+        }
+        composeTestRule.onNodeWithText("Take Photo").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Upload Photo").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Upload Photo").performClick()
+        composeTestRule.waitForIdle()
+
+        // then
+        assertTrue(callbackInvoked)
+    }
+
+    @Test
+    fun shouldNotShowUploadPhotoButtonWhenGalleryIsNotAllowed() {
+        // when
+        composeTestRule.setContent {
+            DocumentCaptureInstructionsScreen(
+                allowPhotoFromGallery = false,
+                onInstructionsAcknowledgedTakePhoto = { },
+            )
+        }
+        composeTestRule.onNodeWithText("Take Photo").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Upload Photo").assertDoesNotExist()
     }
 }
