aa03d733113cf4f0a66cd88006c3684380f552ad
Juma Allan <allanjuma@gmail.com>
2023-05-17 17:16:29 +0300

    added tests for document viewmodel

---
+++ lib/src/test/java/com/smileidentity/viewmodel/DocumentViewModelTest.kt
@@ -0,0 +1,35 @@
+package com.smileidentity.viewmodel
+
+import com.smileidentity.randomJobId
+import com.smileidentity.randomUserId
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class DocumentViewModelTest {
+    private lateinit var subject: DocumentViewModel
+
+    @Before
+    fun setup() {
+        Dispatchers.setMain(Dispatchers.Unconfined)
+        subject = DocumentViewModel(randomUserId(), randomJobId())
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun `uiState should be initialized with the correct defaults`() {
+        val uiState = subject.uiState.value
+        assertEquals(null, uiState.documentImageToConfirm)
+        assertEquals(null, uiState.errorMessage)
+    }
+}
