5f0128045f4599f4bc092b657cfcb3e86cde08cc
Vansh Gandhi <vansh@smileidentity.com>
2023-05-19 16:10:37 -0400

    Directive text positioning

--- lib/src/main/java/com/smileidentity/compose/SelfieCaptureScreen.kt
+++ lib/src/main/java/com/smileidentity/compose/SelfieCaptureScreen.kt
@@ -9,9 +9,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -190,53 +188,50 @@ internal fun SelfieCaptureScreen(
                 textAlign = TextAlign.Center,
                 fontWeight = FontWeight.Bold,
             )
-            Spacer(modifier = Modifier.height(8.dp))
-            AgentModeSwitch(
-                allowAgentMode = allowAgentMode,
-                camSelector = camSelector,
-                onCamSelectorChange = { camSelector = camSelector.inverse },
-            )
+            if (allowAgentMode) {
+                AgentModeSwitch(
+                    camSelector = camSelector,
+                    onCamSelectorChange = { camSelector = camSelector.inverse },
+                )
+            }
         }
     }
 }
 
 @Composable
 private fun AgentModeSwitch(
-    allowAgentMode: Boolean,
     camSelector: CamSelector,
     onCamSelectorChange: (Boolean) -> Unit,
 ) {
-    if (allowAgentMode) {
-        val isAgentModeEnabled = camSelector == CamSelector.Back
-        val agentModeBackgroundColor = if (isAgentModeEnabled) {
-            MaterialTheme.colorScheme.secondary
-        } else {
-            MaterialTheme.colorScheme.surfaceVariant
-        }
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterHorizontally),
-            modifier = Modifier
-                .wrapContentSize()
-                .clip(RoundedCornerShape(32.dp))
-                .background(agentModeBackgroundColor)
-                .padding(8.dp, 0.dp),
-        ) {
-            Text(
-                text = stringResource(R.string.si_agent_mode),
-                color = contentColorFor(agentModeBackgroundColor),
-                style = MaterialTheme.typography.labelLarge,
-                modifier = Modifier.padding(4.dp, 0.dp),
-            )
-            Switch(
-                checked = isAgentModeEnabled,
-                onCheckedChange = onCamSelectorChange,
-                colors = SwitchDefaults.colors(
-                    checkedTrackColor = MaterialTheme.colorScheme.tertiary,
-                ),
-                modifier = Modifier.testTag("agent_mode_switch"),
-            )
-        }
+    val isAgentModeEnabled = camSelector == CamSelector.Back
+    val agentModeBackgroundColor = if (isAgentModeEnabled) {
+        MaterialTheme.colorScheme.secondary
+    } else {
+        MaterialTheme.colorScheme.surfaceVariant
+    }
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterHorizontally),
+        modifier = Modifier
+            .wrapContentSize()
+            .clip(RoundedCornerShape(32.dp))
+            .background(agentModeBackgroundColor)
+            .padding(8.dp, 0.dp),
+    ) {
+        Text(
+            text = stringResource(R.string.si_agent_mode),
+            color = contentColorFor(agentModeBackgroundColor),
+            style = MaterialTheme.typography.labelLarge,
+            modifier = Modifier.padding(4.dp, 0.dp),
+        )
+        Switch(
+            checked = isAgentModeEnabled,
+            onCheckedChange = onCamSelectorChange,
+            colors = SwitchDefaults.colors(
+                checkedTrackColor = MaterialTheme.colorScheme.tertiary,
+            ),
+            modifier = Modifier.testTag("agent_mode_switch"),
+        )
     }
 }
 
