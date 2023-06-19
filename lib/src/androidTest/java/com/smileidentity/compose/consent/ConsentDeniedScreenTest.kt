48d3f528b7ae7e82e1d08208d751dd8d3857d877
Juma Allan <allanjuma@gmail.com>
2023-06-19 16:14:58 +0200

    working on consent denied screen

---
+++ lib/src/androidTest/java/com/smileidentity/compose/consent/ConsentDeniedScreenTest.kt
@@ -0,0 +1,54 @@
+package com.smileidentity.compose.consent
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import com.smileidentity.consent.ConsentDeniedScreen
+import org.junit.Assert
+import org.junit.Rule
+import org.junit.Test
+
+class ConsentDeniedScreenTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun shouldInvokeOnGoBackCallbackOnYesButtonClick() {
+        // given
+        var callbackInvoked = false
+        val onGoBackClicked = { callbackInvoked = true }
+        val yesButtonText = "Yes, go back"
+
+        // when
+        composeTestRule.setContent {
+            ConsentDeniedScreen(
+                onGoBack = onGoBackClicked,
+                onCancel = {},
+            )
+        }
+        composeTestRule.onNodeWithText(yesButtonText).performClick()
+
+        // then
+        Assert.assertTrue(callbackInvoked)
+    }
+
+    @Test
+    fun shouldInvokeOnCancelCallbackOnCancelButtonClick() {
+        // given
+        var callbackInvoked = false
+        val onCancelClicked = { callbackInvoked = true }
+        val cancelButtonText = "No, cancel verification"
+
+        // when
+        composeTestRule.setContent {
+            ConsentDeniedScreen(
+                onGoBack = {},
+                onCancel = onCancelClicked,
+            )
+        }
+        composeTestRule.onNodeWithText(cancelButtonText).performClick()
+
+        // then
+        Assert.assertTrue(callbackInvoked)
+    }
+}
---
+++ lib/src/main/java/com/smileidentity/consent/ConsentDeniedScreen.kt
@@ -0,0 +1,122 @@
+package com.smileidentity.consent
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.smileidentity.R
+import com.smileidentity.compose.SmileIDAttribution
+import com.smileidentity.compose.preview.Preview
+import com.smileidentity.compose.preview.SmilePreview
+
+@Composable
+fun ConsentDeniedScreen(
+    modifier: Modifier = Modifier,
+    onGoBack: () -> Unit,
+    onCancel: () -> Unit,
+) {
+    Surface(
+        modifier = modifier.wrapContentWidth(),
+    ) {
+        Scaffold(
+            content = { innerPadding ->
+                Column(
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .padding(innerPadding)
+                        .verticalScroll(rememberScrollState(), true),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.si_consent_denied),
+                        contentDescription = null,
+                        modifier = modifier.padding(top = 48.dp, bottom = 48.dp),
+                    )
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Text(
+                        text = stringResource(id = R.string.si_consent_consent_denied),
+                        style = MaterialTheme.typography.titleLarge,
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Bold,
+                        color = colorResource(id = R.color.si_color_material_error_container),
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(
+                        text = stringResource(id = R.string.si_consent_consent_denied_title),
+                        style = MaterialTheme.typography.titleSmall,
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(
+                        text = stringResource(id = R.string.si_consent_consent_denied_description),
+                        style = MaterialTheme.typography.titleMedium,
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Bold,
+                    )
+                }
+            },
+            bottomBar = {
+                Column(
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .padding(24.dp)
+                        .verticalScroll(rememberScrollState(), true),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Button(
+                        onClick = onGoBack,
+                        modifier = Modifier
+                            .testTag("consent_screen_denied_go_back_button")
+                            .fillMaxWidth(),
+                    ) { Text(text = stringResource(id = R.string.si_consent_go_back)) }
+
+                    TextButton(
+                        onClick = onCancel,
+                        modifier = Modifier
+                            .testTag("consent_screen_denied_cancel_button")
+                            .fillMaxWidth(),
+                    ) {
+                        Text(
+                            text = stringResource(id = R.string.si_consent_cancel_verification),
+                            color = colorResource(id = R.color.si_color_material_error_container),
+                        )
+                    }
+
+                    SmileIDAttribution()
+                }
+            },
+        )
+    }
+}
+
+@SmilePreview
+@Composable
+fun ConsentDeniedScreenPreview() {
+    Preview {
+        ConsentDeniedScreen(
+            onGoBack = {},
+            onCancel = {},
+        )
+    }
+}
