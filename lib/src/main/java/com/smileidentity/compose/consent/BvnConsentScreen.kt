2915566084f2c6062e9359ff0829d55d008fd5ce
Juma Allan <allanjuma@gmail.com>
2023-06-29 12:33:01 +0200

    added bvn consent screen

---
+++ lib/src/main/java/com/smileidentity/compose/consent/BvnConsentScreen.kt
@@ -0,0 +1,20 @@
+package com.smileidentity.consent
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.smileidentity.compose.preview.Preview
+import com.smileidentity.compose.preview.SmilePreviews
+
+@Composable
+fun BvnConsentScreen(
+    modifier: Modifier = Modifier,
+) {
+}
+
+@SmilePreviews
+@Composable
+private fun BvnConsentScreenPreview() {
+    Preview {
+        BvnConsentScreen()
+    }
+}
