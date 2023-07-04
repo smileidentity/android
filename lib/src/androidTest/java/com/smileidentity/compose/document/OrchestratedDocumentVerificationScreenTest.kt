01888445cf036790d58ae0469e47213756e5c0b9
Juma Allan <allanjuma@gmail.com>
2023-07-04 15:12:18 +0200

    added orchestration screens tests

---
+++ lib/src/androidTest/java/com/smileidentity/compose/document/OrchestratedDocumentVerificationScreenTest.kt
@@ -0,0 +1,50 @@
+package com.smileidentity.compose.document
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsNotDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import com.smileidentity.models.Document
+import org.junit.Rule
+import org.junit.Test
+
+class OrchestratedDocumentVerificationScreenTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    private val document = Document(countryCode = "254", documentType = "NATIONAL_ID")
+
+    @Test
+    fun shouldShowInstructions() {
+        // given
+        val instructionsSubstring = "Submit Front of ID"
+
+        // when
+        composeTestRule.setContent {
+            OrchestratedDocumentVerificationScreen(
+                idType = document,
+            )
+        }
+
+        // then
+        composeTestRule.onNodeWithText(instructionsSubstring, substring = true).assertIsDisplayed()
+    }
+
+    @Test
+    fun shouldNotShowInstructionsWhenDisabled() {
+        // given
+        val instructionsSubstring = "Submit Front of ID"
+
+        // when
+        composeTestRule.setContent {
+            OrchestratedDocumentVerificationScreen(
+                idType = document,
+                showCaptureWithInstructions = false,
+            )
+        }
+
+        // then
+        composeTestRule.onNodeWithText(instructionsSubstring, substring = true)
+            .assertIsNotDisplayed()
+    }
+}
---
+++ lib/src/androidTest/java/com/smileidentity/compose/selfie/OrchestratedSelfieCaptureScreenTest.kt
@@ -0,0 +1,42 @@
+package com.smileidentity.compose.selfie
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsNotDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import org.junit.Rule
+import org.junit.Test
+
+class OrchestratedSelfieCaptureScreenTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun shouldShowInstructions() {
+        // given
+        val instructionsSubstring = "Next, we'll take a quick selfie"
+
+        // when
+        composeTestRule.setContent { OrchestratedSelfieCaptureScreen() }
+
+        // then
+        composeTestRule.onNodeWithText(instructionsSubstring, substring = true).assertIsDisplayed()
+    }
+
+    @Test
+    fun shouldNotShowInstructionsWhenDisabled() {
+        // given
+        val instructionsSubstring = "Next, we'll take a quick selfie"
+
+        // when
+        composeTestRule.setContent {
+            OrchestratedSelfieCaptureScreen(
+                showCaptureWithInstructions = false,
+            )
+        }
+
+        // then
+        composeTestRule.onNodeWithText(instructionsSubstring, substring = true)
+            .assertIsNotDisplayed()
+    }
+}
