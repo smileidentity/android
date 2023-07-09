9dd7a7a450e0a522f78cfc7bcf41c47f02214f52
Juma Allan <allanjuma@gmail.com>
2023-07-09 06:24:59 +0200

    Disable Instructions Screen on the SDK (#142)
    * repackaged selfie tests
    
    * added option to disable instructions screen on the sdk
    
    * split OrchestratedSelfieCaptureScreen from SelfieCaptureScreen
    
    * added orchestration screens tests
    
    * fixed assertions on tests
    
    * renamed show instructions screen param
    
    * cleaning up showing instructions logic
    
    * added missing show instructions
    
    * updated CHANGELOG.md

--- sample/src/main/java/com/smileidentity/sample/compose/MainScreen.kt
+++ sample/src/main/java/com/smileidentity/sample/compose/MainScreen.kt
@@ -199,6 +199,7 @@ fun MainScreen() {
                             SmileID.SmartSelfieEnrollment(
                                 userId = userId,
                                 allowAgentMode = true,
+                                showInstructions = true,
                             ) { result ->
                                 if (result is SmileIDResult.Success) {
                                     val response = result.data.jobStatusResponse
@@ -392,6 +393,7 @@ fun MainScreen() {
                                 userId = userId,
                                 jobId = jobId,
                                 idType = documentType,
+                                showInstructions = true,
                             ) { result ->
                                 if (result is SmileIDResult.Success) {
                                     val resultData = result.data
