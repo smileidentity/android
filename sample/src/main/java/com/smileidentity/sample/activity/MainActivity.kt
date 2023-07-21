26e957bf4ea602e11d37a2ac882044e92d2bc4cb
Juma Allan <allanjuma@gmail.com>
2023-07-21 21:23:29 +0300

    moved theme setup up

--- sample/src/main/java/com/smileidentity/sample/activity/MainActivity.kt
+++ sample/src/main/java/com/smileidentity/sample/activity/MainActivity.kt
@@ -5,16 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.view.WindowCompat
 import com.smileidentity.sample.compose.RootScreen
-import com.smileidentity.sample.compose.SmileIDTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
-            SmileIDTheme {
-                RootScreen()
-            }
+            RootScreen()
         }
     }
 }
