efb04787bf29ee5a4f34cfb4556e63c3e6256a89
Juma Allan <allanjuma@gmail.com>
2023-08-30 22:59:22 +0300

    added qr code to scan button

--- sample/src/main/java/com/smileidentity/sample/compose/components/SmileConfigModalBottomSheet.kt
+++ sample/src/main/java/com/smileidentity/sample/compose/components/SmileConfigModalBottomSheet.kt
@@ -10,7 +10,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedButton
@@ -25,6 +27,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
@@ -93,6 +96,12 @@ fun SmileConfigModalBottomSheet(
                             }
                     },
                 ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_qr_code),
+                        contentDescription = null,
+                        modifier = Modifier.size(ButtonDefaults.IconSize),
+                    )
+                    Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
                     Text(text = stringResource(id = R.string.scan_qr_code))
                 }
                 Button(
