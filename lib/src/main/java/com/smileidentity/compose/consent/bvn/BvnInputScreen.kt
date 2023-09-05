e8a8b18f147c7f7984d06e66d686d7a3f36b38af
Juma Allan <allanjuma@gmail.com>
2023-09-05 16:12:28 +0300

    remove unused imports

--- lib/src/main/java/com/smileidentity/compose/consent/bvn/BvnInputScreen.kt
+++ lib/src/main/java/com/smileidentity/compose/consent/bvn/BvnInputScreen.kt
@@ -14,11 +14,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
