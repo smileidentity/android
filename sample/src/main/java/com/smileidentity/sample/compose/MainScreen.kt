7401527bd7b9d30732ceb1216e7ec4d0833fe6c7
Vansh Gandhi <vansh@smileidentity.com>
2024-02-01 12:33:11 -0800

    Update production chip color

--- sample/src/main/java/com/smileidentity/sample/compose/MainScreen.kt
+++ sample/src/main/java/com/smileidentity/sample/compose/MainScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults.filterChipColors
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -372,6 +373,9 @@ private fun TopBar(
                 selected = uiState.isProduction,
                 onClick = viewModel::toggleEnvironment,
                 label = { Text(stringResource(id = uiState.environmentName)) },
+                colors = filterChipColors(
+                    selectedContainerColor = MaterialTheme.colorScheme.tertiary,
+                ),
             )
             if (isJobsScreenSelected) {
                 PlainTooltipBox(
