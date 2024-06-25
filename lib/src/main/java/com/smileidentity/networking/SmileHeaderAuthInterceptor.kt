8a5324be2d3f167e4f8bea2dad42e42a54117314
Vansh Gandhi <vansh@smileidentity.com>
2024-06-25 10:28:35 -0700

    Updated error handling (#392)
    * Add error handling to background job polling
    
    * Fix Interceptor error handling
    
    * Update CHANGELOG

--- lib/src/main/java/com/smileidentity/networking/SmileHeaderAuthInterceptor.kt
+++ lib/src/main/java/com/smileidentity/networking/SmileHeaderAuthInterceptor.kt
@@ -5,6 +5,7 @@ import com.smileidentity.models.AuthenticationRequest
 import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.Request
+import okio.IOException
 import timber.log.Timber
 
 annotation class SmileHeaderAuth
@@ -20,7 +21,19 @@ object SmileHeaderAuthInterceptor : Interceptor {
         request.getCustomAnnotation(SmileHeaderAuth::class.java) ?: return chain.proceed(request)
         Timber.v("SmileHeaderAuthInterceptor: Interceptor called")
         val authResponse = runBlocking {
-            SmileID.api.authenticate(AuthenticationRequest(enrollment = true))
+            try {
+                SmileID.api.authenticate(AuthenticationRequest(enrollment = true))
+            } catch (e: Exception) {
+                // https://stackoverflow.com/a/58711127/3831060
+                // OkHttp only propagates IOExceptions, so we need to catch HttpException (which can
+                // occur when credentials are not valid, for example) and rethrow it, so that the
+                // exception handlers at the application level can handle it. We add the caught
+                // exception as a suppressed exception to the IOException that we throw, so that
+                // [getExceptionHandler] can still access the original exception.
+                throw IOException("Error authenticating request").apply {
+                    addSuppressed(e)
+                }
+            }
         }
         val newRequest = request.newBuilder()
             .header("SmileID-Partner-ID", SmileID.config.partnerId)
