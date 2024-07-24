fbcff81ca01fc94ddbad534205b6d7567c6c277d
JNdhlovu <JNdhlovu@users.noreply.github.com>
2024-07-24 16:24:27 +0200

    fix: fulldata crash on other types not strings (#419)
    * fix: fulldata crash on other types not strings
    
    * chore: update changelog
    
    * chore: bump version
    
    * chore: bump version

--- lib/src/main/java/com/smileidentity/models/JobStatus.kt
+++ lib/src/main/java/com/smileidentity/models/JobStatus.kt
@@ -9,6 +9,7 @@ import com.smileidentity.networking.calculateSignature
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 import kotlinx.parcelize.Parcelize
+import kotlinx.parcelize.RawValue
 
 @Parcelize
 @JsonClass(generateAdapter = true)
@@ -159,7 +160,7 @@ sealed interface BiometricKycJobResult : JobResult {
         @Json(name = "Address") val address: String?,
         @Json(name = "Country") val country: String?,
         @Json(name = "Document") val documentImageBase64: String?,
-        @Json(name = "FullData") val fullData: Map<String, String>?,
+        @Json(name = "FullData") val fullData: @RawValue Map<String, Any>?,
         @Json(name = "FullName") val fullName: String?,
         @Json(name = "IDNumber") val idNumber: String?,
         @Json(name = "PhoneNumber") val phoneNumber: String?,
@@ -189,7 +190,7 @@ sealed interface EnhancedDocumentVerificationJobResult : JobResult {
         @Json(name = "Address") val address: String?,
         @Json(name = "Country") val country: String?,
         @Json(name = "Document") val documentImageBase64: String?,
-        @Json(name = "FullData") val fullData: Map<String, String>?,
+        @Json(name = "FullData") val fullData: @RawValue Map<String, Any>?,
         @Json(name = "FullName") val fullName: String?,
         @Json(name = "IDNumber") val idNumber: String?,
         @Json(name = "PhoneNumber") val phoneNumber: String?,
