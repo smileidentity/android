a0bf5695e997aa6f8b7372e2b01a13c088eab0be
JNdhlovu <jndhlovu54@gmail.com>
2024-11-20 11:19:53 +0200

    feat: concise map serializer

--- lib/src/main/java/com/smileidentity/compose/nav/PersistentMapSerializer.kt
+++ lib/src/main/java/com/smileidentity/compose/nav/PersistentMapSerializer.kt
@@ -6,29 +6,24 @@ import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.builtins.MapSerializer
 import kotlinx.serialization.descriptors.SerialDescriptor
-import kotlinx.serialization.descriptors.serialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 
 @OptIn(ExperimentalSerializationApi::class)
-class PersistentMapSerializer(
-    private val keySerializer: KSerializer<String>,
-    private val valueSerializer: KSerializer<String>,
-) : KSerializer<ImmutableMap<String, String>> {
+class PersistentMapSerializer<K, V>(
+    keySerializer: KSerializer<K>,
+    valueSerializer: KSerializer<V>,
+) : KSerializer<ImmutableMap<K, V>> {
+    private val mapSerializer = MapSerializer(keySerializer, valueSerializer)
 
-    private class PersistentMapDescriptor :
-        SerialDescriptor by serialDescriptor<Map<String, String>>() {
-        @ExperimentalSerializationApi
-        override val serialName: String = "kotlinx.serialization.immutable.persistentMap"
-    }
+    override val descriptor = SerialDescriptor(
+        "kotlinx.serialization.immutable.persistentMap",
+        mapSerializer.descriptor,
+    )
 
-    override val descriptor: SerialDescriptor = PersistentMapDescriptor()
+    override fun serialize(encoder: Encoder, value: ImmutableMap<K, V>) =
+        mapSerializer.serialize(encoder, value)
 
-    override fun serialize(encoder: Encoder, value: ImmutableMap<String, String>) {
-        return MapSerializer(keySerializer, valueSerializer).serialize(encoder, value)
-    }
-
-    override fun deserialize(decoder: Decoder): ImmutableMap<String, String> {
-        return MapSerializer(keySerializer, valueSerializer).deserialize(decoder).toPersistentMap()
-    }
+    override fun deserialize(decoder: Decoder): ImmutableMap<K, V> =
+        mapSerializer.deserialize(decoder).toPersistentMap()
 }
