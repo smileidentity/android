3ac403c5675d394a181f6c9c5c52574e83eacf7d
Juma Allan <allanjuma@gmail.com>
2025-02-04 10:52:05 +0300

    fixed moshi bug

--- lib/src/main/java/com/smileidentity/models/Upload.kt
+++ lib/src/main/java/com/smileidentity/models/Upload.kt
@@ -57,10 +57,8 @@ data class IdInfo(
 data class ConsentInformation(
     @Json(name = "consent_granted_date") val consentGrantedDate: String,
     @Json(name = "personal_details_consent_granted") val personalDetailsConsentGranted: Boolean,
-    @Json(name = "contact_information_consent_granted") val contactInformationConsentGranted:
-    Boolean,
-    @Json(name = "document_information_consent_granted") val documentInformationConsentGranted:
-    Boolean,
+    @Json(name = "contact_information_consent_granted") val contactInformationConsentGranted: Boolean,
+    @Json(name = "document_information_consent_granted") val documentInformationConsentGranted: Boolean,
 ) : Parcelable
 
 enum class ImageType {
