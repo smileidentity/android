cedaac2c96b5cc1ba472358ae9c907f05b9b54e7
Juma Allan <allanjuma@gmail.com>
2025-02-11 11:49:19 +0300

    fix lint warning

--- lib/src/main/java/com/smileidentity/SmileID.kt
+++ lib/src/main/java/com/smileidentity/SmileID.kt
@@ -30,7 +30,6 @@ import com.smileidentity.networking.PartnerParamsAdapter
 import com.smileidentity.networking.SmartSelfieJobResultAdapter
 import com.smileidentity.networking.SmileHeaderAuthInterceptor
 import com.smileidentity.networking.SmileHeaderMetadataInterceptor
-import com.smileidentity.networking.SmileIDSecurityInterceptor
 import com.smileidentity.networking.SmileIDService
 import com.smileidentity.networking.StringifiedBooleanAdapter
 import com.smileidentity.networking.UploadRequestConverterFactory
