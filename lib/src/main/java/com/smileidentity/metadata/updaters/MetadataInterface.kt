844e755f7de29944b1c109aef29d46074f23ad40
robin <48369660+robin-smileid@users.noreply.github.com>
2025-06-04 23:01:17 +0200

    Fix/expose localmetadata provider (#636)
    * bump version
    
    * expose LocalMetadataProvider public for consumption in flutter capture screens

--- lib/src/main/java/com/smileidentity/metadata/updaters/MetadataInterface.kt
+++ lib/src/main/java/com/smileidentity/metadata/updaters/MetadataInterface.kt
@@ -6,7 +6,7 @@ import androidx.lifecycle.DefaultLifecycleObserver
  * Interface for metadata updaters that can automatically update metadata
  * based on device state or other factors.
  */
-internal interface MetadataInterface : DefaultLifecycleObserver {
+interface MetadataInterface : DefaultLifecycleObserver {
     /**
      * The name of the metadata that this updater manages
      */
