4226a81a2991cbd2ce2c3ea36192598a6c76d37b
Juma Allan <allanjuma@gmail.com>
2025-06-14 12:49:06 +0300

    added hilt plugin

---
+++ smileid-gradle-plugin/plugin/src/main/kotlin/com/smileidentity/plugins/AndroidHiltConventionPlugin.kt
@@ -0,0 +1,26 @@
+package com.smileidentity.plugins
+
+import com.smileidentity.ext.BaseSmileIDPluginExtension
+import com.smileidentity.ext.Convention
+import com.smileidentity.ext.libs
+import org.gradle.kotlin.dsl.dependencies
+
+internal object AndroidHiltConventionPlugin : Convention {
+
+    override fun apply(target: BaseSmileIDPluginExtension<*>) = with(target) {
+        with(project) {
+            with(pluginManager) {
+                apply("com.google.devtools.ksp")
+                apply("dagger.hilt.android.plugin")
+            }
+
+            dependencies {
+                add("implementation", libs.findLibrary("hilt.core").get())
+                add("implementation", libs.findLibrary("hilt.android").get())
+                "ksp"(libs.findLibrary("hilt.compiler").get())
+                add("androidTestImplementation", libs.findLibrary("hilt.android.testing").get())
+                add("testImplementation", libs.findLibrary("hilt.android.testing").get())
+            }
+        }
+    }
+}
