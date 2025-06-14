af5afacd815eee3009e13313245adf848ae5ffdd
Juma Allan <allanjuma@gmail.com>
2025-06-15 01:41:38 +0300

    setting up room migrations

---
+++ sample/src/main/java/com/smileidentity/sample/database/migrations/DatabaseMigrations.kt
@@ -0,0 +1,10 @@
+package com.smileidentity.sample.database.migrations
+
+/**
+ * Automatic schema migrations sometimes require extra instructions to perform the migration, for
+ * example, when a column is renamed. These extra instructions are placed here by creating a class
+ * using the following naming convention `SchemaXtoY` where X is the schema version you're migrating
+ * from and Y is the schema version you're migrating to. The class should implement
+ * `AutoMigrationSpec`.
+ */
+object DatabaseMigrations
