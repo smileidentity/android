cefb1063eea314435120a6136bedd36adf98fb0c
Juma Allan <allanjuma@gmail.com>
2025-06-15 01:40:12 +0300

    cleaning up app class

--- sample/src/main/java/com/smileidentity/sample/SmileIDApplication.kt
+++ sample/src/main/java/com/smileidentity/sample/SmileIDApplication.kt
@@ -3,20 +3,32 @@ package com.smileidentity.sample
 import android.app.Application
 import android.content.Context
 import com.smileidentity.sample.repo.DataStoreRepository
+import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
+@HiltAndroidApp
 class SmileIDApplication : Application() {
+
     override fun onCreate() {
         super.onCreate()
 
-        appContext = this
-        Timber.plant(Timber.DebugTree())
+        appContext = this // todo remove this
+        setupTimber()
 
         // *****Note to Partners*****
         // The line below is how you should initialize the SmileID SDK
         // SmileID.initialize(this)
     }
 
+    private fun setupTimber() = Timber.plant(
+        object : Timber.DebugTree() {
+            override fun createStackElementTag(element: StackTraceElement): String {
+                return super.createStackElementTag(element) + ":" + element.lineNumber
+            }
+        },
+    )
+
+    // todo remove this
     companion object {
         /**
          * This exists only for usage with [DataStoreRepository] since we currently do not use DI.
