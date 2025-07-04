e22cccacabf3d68779ef3f251063555862332a64
John Scott <scottjohn943@gmail.com>
2025-07-04 20:06:17 +0300

    allow custom theme

--- lib/src/main/java/com/smileidentity/compose/theme/experiment/FontProvider.kt
+++ lib/src/main/java/com/smileidentity/compose/theme/experiment/FontProvider.kt
@@ -1,9 +1,11 @@
 package com.smileidentity.compose.theme.experiment
 
+import androidx.compose.ui.text.font.FontFamily
+
 /**
  * Implement this interface to provide custom fonts
  * If you don't, the default material3 theme fonts will be used.
  */
 interface FontProvider {
-    fun getFont(type: TypographyType): SmileFontFamily?
+    fun getFont(type: TypographyType): FontFamily?
 }
