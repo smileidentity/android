7ee7b5747e83a3f32ea91ec1851a198cf5bbd1c4
robin <48369660+robin-smileid@users.noreply.github.com>
2025-08-06 15:28:48 +0100

    Improve emulator check (#697)
    * add build info an move emulator check out
    
    * Update lib/src/main/java/com/smileidentity/metadata/updaters/BuildInfoMetadata.kt
    
    add try catch block
    
    Co-authored-by: prfectionist[bot] <187910262+prfectionist[bot]@users.noreply.github.com>
    
    * fix file
    
    ---------
    
    Co-authored-by: prfectionist[bot] <187910262+prfectionist[bot]@users.noreply.github.com>

--- lib/src/main/java/com/smileidentity/metadata/device/DeviceInformation.kt
+++ lib/src/main/java/com/smileidentity/metadata/device/DeviceInformation.kt
@@ -4,46 +4,15 @@ import android.os.Build
 import com.smileidentity.SmileIDCrashReporting
 import timber.log.Timber
 
-private val isEmulator: Boolean
-    get() {
-        try {
-            return (
-                (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic")) ||
-                    Build.FINGERPRINT.startsWith("generic") ||
-                    Build.FINGERPRINT.startsWith("unknown") ||
-                    Build.HARDWARE.contains("goldfish") ||
-                    Build.HARDWARE.contains("ranchu") ||
-                    Build.MODEL.contains("google_sdk") ||
-                    Build.MODEL.contains("Emulator") ||
-                    Build.MODEL.contains("Android SDK built for x86") ||
-                    Build.MANUFACTURER.contains("Genymotion") ||
-                    Build.PRODUCT.contains("sdk_google") ||
-                    Build.PRODUCT.contains("google_sdk") ||
-                    Build.PRODUCT.contains("sdk") ||
-                    Build.PRODUCT.contains("sdk_x86") ||
-                    Build.PRODUCT.contains("sdk_gphone64_arm64") ||
-                    Build.PRODUCT.contains("vbox86p") ||
-                    Build.PRODUCT.contains("emulator") ||
-                    Build.PRODUCT.contains("simulator")
-                )
-        } catch (e: Exception) {
-            Timber.e(e)
-            return false
-        }
-    }
-
 /**
- * Returns the model of the device. If the device is an emulator, it returns "emulator". Any errors
- * result in "unknown"
+ * Returns the model of the device. Any errors result in "unknown"
  */
 internal val model: String
     get() {
         try {
             val manufacturer = Build.MANUFACTURER
             val model = Build.MODEL
-            return if (isEmulator) {
-                "emulator"
-            } else if (model.contains(manufacturer, ignoreCase = true)) {
+            return if (model.contains(manufacturer, ignoreCase = true)) {
                 model
             } else {
                 "$manufacturer $model"
