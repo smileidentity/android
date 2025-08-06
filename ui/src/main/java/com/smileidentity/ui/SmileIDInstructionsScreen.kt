8c4cb2a68850852579a79bd44ca590eb35b30e15
Juma Allan <allanjuma@gmail.com>
2025-08-06 18:49:07 +0300

    setting up instructions column

--- ui/src/main/java/com/smileidentity/ui/SmileIDInstructionsScreen.kt
+++ ui/src/main/java/com/smileidentity/ui/SmileIDInstructionsScreen.kt
@@ -1,13 +1,21 @@
 package com.smileidentity.ui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.smileidentity.ui.components.SmileIDButton
 
 @Composable
@@ -24,8 +32,31 @@ fun SmileIDInstructionsScreen(
         )
     },
 ) {
-    Column(modifier = modifier) {
-        button(onContinueClick)
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top,
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth()
+                .verticalScroll(rememberScrollState())
+                .weight(1f),
+        ) {
+        }
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+        ) {
+            button(onContinueClick)
+        }
     }
 }
 
