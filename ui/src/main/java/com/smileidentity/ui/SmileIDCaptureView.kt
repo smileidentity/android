659819f1ec106df55ca7445223a96b86da98e014
Juma Allan <allanjuma@gmail.com>
2025-08-07 17:42:09 +0300

    mock capture view

--- ui/src/main/java/com/smileidentity/ui/SmileIDCaptureView.kt
+++ ui/src/main/java/com/smileidentity/ui/SmileIDCaptureView.kt
@@ -1,17 +1,36 @@
 package com.smileidentity.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.unit.dp
 import com.smileidentity.ml.states.IdentityScanState
-import com.smileidentity.ui.components.SmileCameraPreview
 import com.smileidentity.ui.components.DocumentShapedView
 import com.smileidentity.ui.components.FaceShapedView
+import com.smileidentity.ui.components.SmileCameraPreview
+import com.smileidentity.ui.components.SmileIDButton
 import java.io.File
 
 @Composable
 fun SmileIDCaptureView(
     scanType: IdentityScanState.ScanType,
     modifier: Modifier = Modifier,
+    continueButton: @Composable (onResult: (File) -> Unit) -> Unit = { onResult ->
+        SmileIDButton(
+            text = "Continue",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(all = 16.dp)
+                .testTag(tag = "capture:continue_button"),
+            onClick = { onResult },
+        )
+    },
     onResult: (File) -> Unit,
 ) {
     SmileCameraPreview(
@@ -28,6 +47,15 @@ fun SmileIDCaptureView(
             IdentityScanState.ScanType.DOCUMENT_FRONT -> DocumentShapedView()
             IdentityScanState.ScanType.DOCUMENT_BACK -> DocumentShapedView()
             IdentityScanState.ScanType.SELFIE -> FaceShapedView()
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Bottom,
+        ) {
+            continueButton {}
         }
     }
 }
