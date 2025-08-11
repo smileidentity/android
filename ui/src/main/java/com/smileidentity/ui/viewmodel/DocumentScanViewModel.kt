a03e197961a35fe067b6febb50659fa0ec2c4ea2
Juma Allan <allanjuma@gmail.com>
2025-08-12 00:04:58 +0300

    setting up identity scanners

---
+++ ui/src/main/java/com/smileidentity/ui/viewmodel/DocumentScanViewModel.kt
@@ -0,0 +1,15 @@
+package com.smileidentity.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+
+class DocumentScanViewModel : IdentityScanViewModel() {
+
+    class IdentityScanViewModelFactory() : ViewModelProvider.Factory {
+        @Suppress("UNCHECKED_CAST")
+        override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
+            return DocumentScanViewModel() as T
+        }
+    }
+}
---
+++ ui/src/main/java/com/smileidentity/ui/viewmodel/SelfieScanViewModel.kt
@@ -0,0 +1,15 @@
+package com.smileidentity.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+
+class SelfieScanViewModel : IdentityScanViewModel() {
+
+    class SelfieScanViewModelFactory() : ViewModelProvider.Factory {
+        @Suppress("UNCHECKED_CAST")
+        override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
+            return SelfieScanViewModel() as T
+        }
+    }
+}
