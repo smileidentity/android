a3b8f9678d0fc53265f83d918aa1fb73e88facd4
Juma Allan <allanjuma@gmail.com>
2025-08-12 21:05:39 +0300

    Check Device Root Status 🐓  (#694)
    * added rootbeer
    
    * updated device root status
    
    * updated root detection

--- lib/src/main/java/com/smileidentity/networking/SmileHeaderMetadataInterceptor.kt
+++ lib/src/main/java/com/smileidentity/networking/SmileHeaderMetadataInterceptor.kt
@@ -1,6 +1,7 @@
 package com.smileidentity.networking
 
 import com.smileidentity.BuildConfig
+import com.smileidentity.SmileID
 import com.smileidentity.util.getCurrentIsoTimestamp
 import okhttp3.Interceptor
 
@@ -13,6 +14,7 @@ object SmileHeaderMetadataInterceptor : Interceptor {
             ?: return chain.proceed(request)
         val newRequest = request.newBuilder()
             .header("Connection", "Keep-Alive")
+            .header("SmileID-loader", SmileID.loader)
             .header("SmileID-Source-SDK", "android")
             .header("SmileID-Source-SDK-Version", BuildConfig.VERSION_NAME)
             .header("SmileID-Request-Timestamp", getCurrentIsoTimestamp())
