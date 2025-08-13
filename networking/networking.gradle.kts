937b632617ff00994fbb719cd839b225bb8827e2
Juma Allan <allanjuma@gmail.com>
2025-08-14 00:37:43 +0300

    Setting up Networking Layer :neckbeard:  (#714)
    * removed ui test directory
    
    * added kotlinx serialization and ksp
    
    * setting up SmileAPI
    
    * lint fixes
    
    * added kotlinx serialization adapter
    
    * added GZIP and Header Auth interceptors
    
    * setting up Json and Retrofit
    
    * setting up Json and Retrofit
    
    * setting up Json and Retrofit
    
    * lint fixes

--- networking/networking.gradle.kts
+++ networking/networking.gradle.kts
@@ -1,8 +1,24 @@
 plugins {
     alias(libs.plugins.smileid.android.library)
     alias(libs.plugins.smileid.android.library.jacoco)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
     namespace = "com.smileidentity.networking"
+}
+
+dependencies {
+    // OkHttp is exposed in public SmileID interface (initialize), hence "api" vs "implementation"
+    api(libs.okhttp)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.scalars)
+    implementation(libs.retrofit.converter.kotlinx.serialization)
+    implementation(libs.okhttp.logging.interceptor)
+
+    implementation(libs.kotlinx.serialization.json)
+
+    testImplementation(libs.okhttp.mockwebserver)
+    testImplementation(libs.mockk)
 }
---
+++ networking/src/test/java/com/smileidentity/networking/interceptor/GzipRequestInterceptorTest.kt
@@ -0,0 +1,103 @@
+package com.smileidentity.networking.interceptor
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.slot
+import io.mockk.verify
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import okhttp3.Interceptor
+import okhttp3.Request
+
+class GzipRequestInterceptorTest {
+    private val subject = GzipRequestInterceptor()
+
+    @Test
+    fun `apply gzip encoding`() {
+        // given
+        val request =
+            Request.Builder()
+                .url("http://api.smileidentity.com")
+                .post(mockk())
+                .build()
+        val chain = mockk<Interceptor.Chain>()
+        val capturedRequest = slot<Request>()
+        every { chain.request() } returns request
+        every { chain.proceed(capture(capturedRequest)) } returns mockk()
+
+        // when
+        subject.intercept(chain)
+
+        // then
+        verify { chain.proceed(any()) }
+        assertNotNull(capturedRequest.captured.body)
+        assertEquals("gzip", capturedRequest.captured.header("Content-Encoding"))
+        assertEquals(-1L, capturedRequest.captured.body?.contentLength())
+        assertEquals(request.method, capturedRequest.captured.method)
+        assertEquals(request.url, capturedRequest.captured.url)
+    }
+
+    @Test
+    fun `skip if not smile identity host`() {
+        // given
+        val request =
+            Request.Builder()
+                .url("http://www.google.com")
+                .post(mockk())
+                .build()
+        val chain = mockk<Interceptor.Chain>()
+        val capturedRequest = slot<Request>()
+        every { chain.request() } returns request
+        every { chain.proceed(capture(capturedRequest)) } returns mockk()
+
+        // when
+        subject.intercept(chain)
+
+        // then
+        verify { chain.proceed(any()) }
+        assert(capturedRequest.captured.url.host != "api.smileidentity.com")
+    }
+
+    @Test
+    fun `skip if encoding already set`() {
+        // given
+        val request =
+            Request.Builder()
+                .url("http://api.smileidentity.com")
+                .header("Content-Encoding", "identity")
+                .build()
+        val chain = mockk<Interceptor.Chain>()
+        val capturedRequest = slot<Request>()
+        every { chain.request() } returns request
+        every { chain.proceed(capture(capturedRequest)) } returns mockk()
+
+        // when
+        subject.intercept(chain)
+
+        // then
+        verify { chain.proceed(any()) }
+        assert(capturedRequest.captured.header("Content-Encoding") == "identity")
+    }
+
+    @Test
+    fun `skip if body is null`() {
+        // given
+        val request =
+            Request.Builder()
+                .url("http://api.smileidentity.com")
+                .build()
+        val chain = mockk<Interceptor.Chain>()
+        val capturedRequest = slot<Request>()
+        every { chain.request() } returns request
+        every { chain.proceed(capture(capturedRequest)) } returns mockk()
+
+        // when
+        subject.intercept(chain)
+
+        // then
+        verify { chain.proceed(any()) }
+        assert(capturedRequest.captured.body == null)
+        assert(capturedRequest.captured.header("Content-Encoding") == null)
+    }
+}
