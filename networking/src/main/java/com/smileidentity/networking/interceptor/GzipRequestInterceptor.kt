7a09d0fca33eaa65a3df986af326f5d6965b3fc2
Juma Allan <allanjuma@gmail.com>
2025-08-13 20:17:14 +0300

    added GZIP and Header Auth interceptors

---
+++ networking/src/main/java/com/smileidentity/networking/interceptor/GzipRequestInterceptor.kt
@@ -0,0 +1,43 @@
+package com.smileidentity.networking.interceptor
+
+import okhttp3.Interceptor
+import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.Response
+import okio.BufferedSink
+import okio.GzipSink
+import okio.buffer
+import okio.use
+
+/**
+ * This interceptor compresses the HTTP request body. It is included by default for all SmileID
+ * requests. If this is used for other websites, it is possible they may not support gzip encoding.
+ */
+internal class GzipRequestInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest: Request = chain.request()
+        val body = originalRequest.body
+        val isValidHost = originalRequest.url.host.contains("api.smileidentity.com")
+        if (body == null || originalRequest.header("Content-Encoding") != null || !isValidHost) {
+            return chain.proceed(originalRequest)
+        }
+
+        val compressedRequest: Request =
+            originalRequest.newBuilder()
+                .header("Content-Encoding", "gzip")
+                .method(originalRequest.method, gzip(body))
+                .build()
+        return chain.proceed(compressedRequest)
+    }
+
+    private fun gzip(body: RequestBody): RequestBody = object : RequestBody() {
+        override fun contentType() = body.contentType()
+
+        // We don't know the compressed length in advance
+        override fun contentLength(): Long = -1
+
+        override fun writeTo(sink: BufferedSink) = GzipSink(sink).buffer().use {
+            body.writeTo(it)
+        }
+    }
+}
