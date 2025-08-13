d8afbfd84653c79f8d946426e7d72591be8dc34b
Juma Allan <allanjuma@gmail.com>
2025-08-13 16:36:07 +0300

    added kotlinx serialization and ksp

--- networking/networking.gradle.kts
+++ networking/networking.gradle.kts
@@ -1,8 +1,14 @@
 plugins {
     alias(libs.plugins.smileid.android.library)
     alias(libs.plugins.smileid.android.library.jacoco)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
     namespace = "com.smileidentity.networking"
+}
+
+dependencies {
+    implementation(libs.kotlinx.serialization.json)
 }
