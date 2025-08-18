e8628427bad04dbf49d74d50e327e0b37da19b5b
Robin Ankele <robin@smileidentity.com>
2025-08-18 11:22:28 +0100

    add packagename and certificate digest to the build info

--- lib/src/main/java/com/smileidentity/metadata/updaters/BuildInfoMetadata.kt
+++ lib/src/main/java/com/smileidentity/metadata/updaters/BuildInfoMetadata.kt
@@ -1,12 +1,17 @@
 package com.smileidentity.metadata.updaters
 
 import android.content.Context
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.content.pm.Signature
 import android.os.Build
+import android.util.Base64
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import com.smileidentity.metadata.models.MetadataKey
 import com.smileidentity.metadata.models.Metadatum
 import com.smileidentity.metadata.models.Value
 import com.smileidentity.metadata.updateOrAddBy
+import java.security.MessageDigest
 
 /**
  * A manager that updates metadata with build information.
@@ -25,7 +30,39 @@ internal class BuildInfoMetadata(
         forceUpdate()
     }
 
-    private fun getBuildInfo(): Map<String, Value> {
+    private fun getCertificateSha256Digests(): List<String> {
+        val packageInfo: PackageInfo = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            packageManager.getPackageInfo(
+                packageName,
+                PackageManager.GET_SIGNING_CERTIFICATES,
+            )
+        } else {
+            packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
+        }
+
+        val signatures: Array<out Signature?>? = if (Build.VERSION.SDK_INT >=
+            Build.VERSION_CODES.P
+        ) {
+            packageInfo.signingInfo?.apkContentsSigners
+        } else {
+            packageInfo.signatures
+        }
+
+        if (signatures == null) return emptyList()
+
+        val md = MessageDigest.getInstance("SHA-256")
+        return signatures.mapNotNull { signature ->
+            signature?.toByteArray()?.let { cert ->
+                val digest = md.digest(cert)
+                Base64.encodeToString(
+                    digest,
+                    Base64.NO_PADDING or Base64.NO_WRAP or Base64.URL_SAFE,
+                )
+            }
+        }
+    }
+
+    private fun getBuildSource(): String {
         val buildSource = try {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
                 // Android 11 (API 30) and above
@@ -37,7 +74,12 @@ internal class BuildInfoMetadata(
         } catch (e: Exception) {
             "unknown"
         }
+        return buildSource
+    }
 
+    private fun getBuildInfo(): Map<String, Value> {
+        val buildSource = getBuildSource()
+        val certificateDigests = getCertificateSha256Digests()
         return mapOf(
             "brand" to Value.StringValue(Build.BRAND),
             "device" to Value.StringValue(Build.DEVICE),
@@ -45,6 +87,10 @@ internal class BuildInfoMetadata(
             "hardware" to Value.StringValue(Build.HARDWARE),
             "product" to Value.StringValue(Build.PRODUCT),
             "build_source" to Value.StringValue(buildSource),
+            "package_name" to Value.StringValue(packageName),
+            "certificate_digest" to Value.ArrayValue(
+                certificateDigests.map { Value.StringValue(it) },
+            ),
         )
     }
 
