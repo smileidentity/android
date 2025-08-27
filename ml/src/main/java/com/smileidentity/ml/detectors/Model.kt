21c8ca79ca0b96e151681144810de7e98b642c75
Juma Allan <allanjuma@gmail.com>
2025-08-27 17:30:54 +0300

    updated analyzer input

--- ml/src/main/java/com/smileidentity/ml/detectors/Model.kt
+++ ml/src/main/java/com/smileidentity/ml/detectors/Model.kt
@@ -1,20 +1,13 @@
 package com.smileidentity.ml.detectors
 
 import android.graphics.Bitmap
-import android.graphics.Rect
 import com.google.mediapipe.tasks.vision.facedetector.FaceDetectorResult
 import com.google.mediapipe.tasks.vision.objectdetector.ObjectDetectorResult
 
 /**
  * Input from CameraAdapter, note: the bitmap should already be encoded in RGB value
  */
-data class AnalyzerInput(val viewFinderBounds: Rect, val image: Bitmap)
-
-/**
- * Result bounding box coordinates of DocumentDetector/FaceDetector,
- * in percentage values with regard to original image's width/height
- */
-data class BoundingBox(val left: Float, val top: Float, val width: Float, val height: Float)
+data class AnalyzerInput(val image: Bitmap)
 
 /**
  * Output interface of ML models
