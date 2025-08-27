2eb86ccb6df591b5ad8798e22495627f08a42bed
Juma Allan <allanjuma@gmail.com>
2025-08-27 17:30:00 +0300

    setting up mediapipe

--- ml/src/main/java/com/smileidentity/ml/detectors/DocumentDetectorAnalyzer.kt
+++ ml/src/main/java/com/smileidentity/ml/detectors/DocumentDetectorAnalyzer.kt
@@ -1,19 +1,34 @@
 package com.smileidentity.ml.detectors
 
+import android.content.Context
+import com.google.mediapipe.framework.image.BitmapImageBuilder
+import com.google.mediapipe.tasks.core.BaseOptions
+import com.google.mediapipe.tasks.vision.core.RunningMode
+import com.google.mediapipe.tasks.vision.objectdetector.ObjectDetector
 import com.smileidentity.camera.Analyzer
 import com.smileidentity.ml.states.IdentityScanState
 
 /**
  * Analyzer to run DocumentDetector.
  */
-class DocumentDetectorAnalyzer : Analyzer<AnalyzerInput, IdentityScanState, AnalyzerOutput> {
+class DocumentDetectorAnalyzer(context: Context) :
+    Analyzer<AnalyzerInput, IdentityScanState, AnalyzerOutput> {
 
+    val baseOptions: ObjectDetector.ObjectDetectorOptions =
+        ObjectDetector.ObjectDetectorOptions.builder()
+            .setBaseOptions(BaseOptions.builder().setModelAssetPath("modelName").build())
+            .setRunningMode(RunningMode.IMAGE)
+            .setMaxResults(5)
+            .build()
+    val objectDetector: ObjectDetector = ObjectDetector.createFromOptions(context, baseOptions)
+
+    /**
+     * we will run document detection using MediaPipe and pass back the output
+     *
+     */
     override suspend fun analyze(data: AnalyzerInput, state: IdentityScanState): AnalyzerOutput {
-        TODO("Not yet implemented")
-
-        /**
-         * we will run document detection here, using Tensorflow and pass back the output
-         *
-         */
+        val image = BitmapImageBuilder(data.image).build()
+        val result = objectDetector.detect(image)
+        return DocumentDetectorOutput(result = result)
     }
 }
