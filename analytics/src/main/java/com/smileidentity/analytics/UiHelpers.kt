3a34983f3855e688ce4861fbbeeb2ecb03218f6d
Juma Allan <allanjuma@gmail.com>
2025-08-29 14:35:36 +0300

    setting up analytics

---
+++ analytics/src/main/java/com/smileidentity/analytics/UiHelpers.kt
@@ -0,0 +1,14 @@
+package com.smileidentity.analytics
+
+import android.annotation.SuppressLint
+import androidx.compose.runtime.staticCompositionLocalOf
+
+/**
+ * Global key used to obtain access to the AnalyticsHelper through a CompositionLocal.
+ */
+@SuppressLint("ComposeCompositionLocalUsage")
+val LocalAnalyticsHelper = staticCompositionLocalOf<AnalyticsHelper> {
+    // Provide a default AnalyticsHelper which does nothing. This is so that tests and previews
+    // do not have to provide one. For real app builds provide a different implementation.
+    NoOpAnalyticsHelper()
+}
