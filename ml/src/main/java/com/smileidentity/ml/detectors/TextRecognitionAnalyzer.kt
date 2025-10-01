110665e22160a53060e084e53dcbd8f7c7ec8a71
Juma Allan <allanjuma@gmail.com>
2025-10-01 22:48:18 +0200

    added text recognition for doc ocr

--- ml/src/main/java/com/smileidentity/ml/detectors/TextRecognitionAnalyzer.kt
+++ ml/src/main/java/com/smileidentity/ml/detectors/TextRecognitionAnalyzer.kt
@@ -1,34 +1,39 @@
 package com.smileidentity.ml.detectors
 
-import android.content.Context
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 import com.smileidentity.camera.Analyzer
 import com.smileidentity.camera.AnalyzerFactory
 import com.smileidentity.ml.model.AnalyzerInput
 import com.smileidentity.ml.model.AnalyzerOutput
+import com.smileidentity.ml.model.TextRecognitionOutput
 import com.smileidentity.ml.states.IdentityScanState
 
-class TextRecognitionAnalyzer() : Analyzer<AnalyzerInput, IdentityScanState, AnalyzerOutput> {
+class TextRecognitionAnalyzer : Analyzer<AnalyzerInput, IdentityScanState, AnalyzerOutput> {
 
-    override suspend fun analyze(
-        data: AnalyzerInput,
-        state: IdentityScanState,
-    ): AnalyzerOutput {
+    val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
 
+    override suspend fun analyze(data: AnalyzerInput, state: IdentityScanState): AnalyzerOutput {
+        val cameraFrameBitmap = data.cameraPreviewImage.image
+        val image = InputImage.fromBitmap(cameraFrameBitmap, 0)
+        val result = recognizer.process(image)
+        return TextRecognitionOutput(text = result.result)
     }
 
-    class Factory() :
+    class Factory :
         AnalyzerFactory<
-                AnalyzerInput,
-                IdentityScanState,
-                AnalyzerOutput,
-                Analyzer<AnalyzerInput, IdentityScanState, AnalyzerOutput>,
-                > {
+            AnalyzerInput,
+            IdentityScanState,
+            AnalyzerOutput,
+            Analyzer<AnalyzerInput, IdentityScanState, AnalyzerOutput>,
+            > {
 
         override suspend fun newInstance(): Analyzer<
-                AnalyzerInput,
-                IdentityScanState,
-                AnalyzerOutput,
-                > =
+            AnalyzerInput,
+            IdentityScanState,
+            AnalyzerOutput,
+            > =
             TextRecognitionAnalyzer()
     }
 }
